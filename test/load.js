95f43a38c07ba20b5436b8612c547a41808e3b49
Mike Bostock <mbostock@gmail.com>
2013-06-23 09:59:21 -0700

    Fix tests for relocated source files.

--- test/load.js
+++ test/load.js
@@ -2,10 +2,10 @@ var smash = require("smash"),
     d3 = require("d3");
 
 module.exports = function() {
-  var files = [].slice.call(arguments),
+  var files = [].slice.call(arguments).map(function(d) { return "src/" + d; }),
       sandbox = {d3: d3};
-  files.unshift("start");
-  files.push("end");
+  files.unshift("src/start");
+  files.push("src/end");
 
   function topic() {
     smash.load(files, "d3.geo", sandbox, this.callback);
