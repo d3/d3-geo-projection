984df57bce0d3fba8aac07e6ab8dd566b556dce6
Fil <fil@rezo.net>
2016-07-03 12:22:27 +0200

    rectangularPolyconic v4

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ export {default as geoKavrayskiy7, kavrayskiy7Raw as geoKavrayskiy7Raw} from "./
 export {default as geoMollweide, mollweideRaw as geoMollweideRaw} from "./src/mollweide";
 export {default as geoPierceQuincuncial} from "./src/pierceQuincuncial.js";
 export {default as geoPolyconic, polyconicRaw as geoPolyconicRaw} from "./src/polyconic";
+export {default as geoRectangularPolyconic, rectangularPolyconicRaw as geoRectangularPolyconicRaw} from "./src/rectangularPolyconic";
 export {default as geoRobinson, robinsonRaw as geoRobinsonRaw} from "./src/robinson";
 export {default as geoSinusoidal, sinusoidalRaw as geoSinusoidalRaw} from "./src/sinusoidal";
 export {default as geoTimes, timesRaw as geoTimesRaw} from "./src/times";
