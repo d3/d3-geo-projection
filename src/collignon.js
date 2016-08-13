bbe2579dd561cac555bf87d757af74c4225f2fdd
Fil <fil@rezo.net>
2016-08-13 11:00:32 +0200

    make collignon mutable so I can adjust x by a factor 2/sqrt(3) ; I think this is not the correct method, but I can't find a generic way to compose projections

--- src/collignon.js
+++ src/collignon.js
@@ -1,18 +1,30 @@
-import {geoProjection as projection} from "d3-geo";
+import {geoProjectionMutator as projectionMutator} from "d3-geo";
 import {asin, pi, sin, sqrt, sqrtPi} from "./math";
 
-export function collignonRaw(lambda, phi) {
-  var alpha = sqrt(1 - sin(phi));
-  return [(2 / sqrtPi) * lambda * alpha, sqrtPi * (1 - alpha)];
+export function collignonRaw(beta) {
+  function forward (lambda, phi) {
+    var alpha = sqrt(1 - sin(phi));
+    return [beta * (2 / sqrtPi) * lambda * alpha, sqrtPi * (1 - alpha)];
+  };
+  forward.invert = function(x, y) {
+    x /= beta;
+    var lambda = (lambda = y / sqrtPi - 1) * lambda;
+    return [lambda > 0 ? x * sqrt(pi / lambda) / 2 : 0, asin(1 - lambda)];
+  };
+  return forward;
 }
 
-collignonRaw.invert = function(x, y) {
-  var lambda = (lambda = y / sqrtPi - 1) * lambda;
-  return [lambda > 0 ? x * sqrt(pi / lambda) / 2 : 0, asin(1 - lambda)];
-};
 
 export default function() {
-  return projection(collignonRaw)
+  var beta = 1,
+      m = projectionMutator(collignonRaw),
+      p = m(beta);
+
+  p.distort = function(_) {
+    return arguments.length ? m(beta = _) : beta;
+  };
+
+  return p
       .scale(95.6464)
       .center([0, 30]);
 }
