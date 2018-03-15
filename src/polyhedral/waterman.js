83a9d2f5d063550c91dbd9d8b54f56ae130c2a69
Philippe Rivière <fil@rezo.net>
2018-03-15 01:43:35 +0100

    whitespace

--- src/polyhedral/waterman.js
+++ src/polyhedral/waterman.js
@@ -86,7 +86,7 @@ export default function(faceProjection) {
   return polyhedral(faces[0], face)
       .angle(-30)
       .scale(110.625)
-      .center([0, 45]);
+      .center([0,45]);
 }
 
 function dot(a, b) {
