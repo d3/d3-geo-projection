8c8dcedb284a5930f2c701e64eedfbbb95dc0fa4
Fil <fil@rezo.net>
2018-08-20 16:45:24 +0200

    geoproject: contains() and clockwise() : difficult cases must be delegated to d3-geo's slower but correct functions

--- src/project/clockwise.js
+++ src/project/clockwise.js
@@ -1,8 +1,25 @@
+import {geoArea} from "d3-geo";
+import {abs, tau} from "../math.js";
+
 export default function(ring) {
   if ((n = ring.length) < 4) return false;
   var i = 0,
-      n,
-      area = ring[n - 1][1] * ring[0][0] - ring[n - 1][0] * ring[0][1];
-  while (++i < n) area += ring[i - 1][1] * ring[i][0] - ring[i - 1][0] * ring[i][1];
+    n,
+    a0 = ring[n - 1][0],
+    a1 = ring[n - 1][1],
+    b0 = ring[0][0],
+    b1 = ring[0][1];
+  if (abs(a0 - b0) >= 180)
+    return geoArea({ type: "Polygon", coordinates: [ring] }) < tau;
+  var area = a1 * b0 - a0 * b1;
+  while (++i < n) {
+    a0 = b0;
+    a1 = b1;
+    b0 = ring[i][0];
+    b1 = ring[i][1];
+    if (abs(a0 - b0) >= 180)
+      return geoArea({ type: "Polygon", coordinates: [ring] }) < tau;
+    area += a1 * b0 - a0 * b1;
+  }
   return area >= 0;
 }
--- test/project-test.js
+++ test/project-test.js
@@ -2,18 +2,37 @@ var tape = require("tape"),
     d3 = Object.assign({}, require("d3-geo"), require("../"));
 
 tape("project(polygon with hole) preserves the hole", function(test) {
-  test.deepEqual(d3.geoProject({
+  var poly = {
     type: "Polygon",
     coordinates: [
       [[0, 0], [0, 4], [4, 4], [4, 0], [0, 0]],
       [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]]
     ]
-  }, d3.geoIdentity()), {
+  };
+  test.deepEqual(d3.geoProject(poly, d3.geoIdentity()), poly);
+  test.end();
+});
+
+tape("project(polygon & hole on the antimeridian) preserves the polygon", function(test) {
+  var poly = {
     type: "Polygon",
     coordinates: [
-      [[0, 0], [0, 4], [4, 4], [4, 0], [0, 0]],
-      [[1, 1], [2, 1], [2, 2], [1, 2], [1, 1]]
+      [[178, 0], [-178, 2], [-178, -2], [178, 0]],
+      [[179, 0], [-179, -1], [-179, 1], [179, 0]]
     ]
-  });
+  };
+  test.deepEqual(d3.geoProject(poly, d3.geoIdentity()), poly);
+  test.end();
+});
+
+tape("project(polygon & hole at the pole) preserves the polygon", function(test) {
+  var poly = {
+    type: "Polygon",
+    coordinates: [
+      [[180, -85], [-90, -85], [0, -85], [90, -85], [180, -85]],
+      [[180, -89], [90, -89], [0, -89], [-90, -89], [180, -89]]
+    ]
+  };
+  test.deepEqual(d3.geoProject(poly, d3.geoIdentity()), poly);
   test.end();
 });
