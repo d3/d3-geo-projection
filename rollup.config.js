89cca65529229600a0fdfe20bce3d0b8b7fc5da9
Mike Bostock <mbostock@gmail.com>
2018-08-24 13:13:00 -0700

    Update dependencies.

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,36 @@
-export default {
-  input: "index",
-  external: [
-    "d3-array",
-    "d3-geo"
-  ],
+import {terser} from "rollup-plugin-terser";
+import * as meta from "./package.json";
+
+const config = {
+  input: "src/index.js",
+  external: Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)),
   output: {
-    extend: true,
-    file: "build/d3-geo-projection.js",
+    file: `dist/${meta.name}.js`,
+    name: "d3",
     format: "umd",
-    globals: {
-      "d3-array": "d3",
-      "d3-geo": "d3"
-    },
-    name: "d3"
-  }
+    indent: false,
+    extend: true,
+    banner: `// ${meta.homepage} v${meta.version} Copyright ${(new Date).getFullYear()} ${meta.author.name}`,
+    globals: Object.assign({}, ...Object.keys(meta.dependencies || {}).filter(key => /^d3-/.test(key)).map(key => ({[key]: "d3"})))
+  },
+  plugins: []
 };
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: `dist/${meta.name}.min.js`
+    },
+    plugins: [
+      ...config.plugins,
+      terser({
+        output: {
+          preamble: config.output.banner
+        }
+      })
+    ]
+  }
+];
